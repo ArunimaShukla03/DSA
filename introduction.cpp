3adbde9b909cf486e2f84924798f73cda136b145
ArunimaShukla03 <arunimashukla2003@gmail.com>
2022-11-01 19:26:16 +0530

    introduction to dsa

---
+++ introduction.cpp
@@ -0,0 +1,23 @@
+''' 
+
+a program is set of instructions which performs operations on data. When a program is dealing with data, then the way the data is organised in the memory is called "DATA STRUCTURES" OR the way you organise data in main memory during execution of program is called "DATA STRUCTURES"
+
+Depending on requirements, we may choose data structures. Data structures in C++ are:
+- arrays
+- matrices
+- linked list
+- stack
+- queue
+- trees
+- graphs
+- hashing
+- recursion
+- sorting
+
+Here, 
+
+Arrays, matrices and linked list are physical data structures which define how data is arranged in the memory.
+
+Stack, queue, trees, graphs and hashing are logical data structures which define how data is utilised in the memory.
+
+'''
